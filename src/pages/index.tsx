e69ea3db332ae9705770980ac2123aaa99507c24
Tarcísio Delmondes <tarcisiodelmondes@gmail.com>
2021-12-17 09:21:48 -0300

    Refactor Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,17 @@
-import { Box, Text, useBreakpointValue, Image } from "@chakra-ui/react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import Link from "next/link";
+import { Box, Text, useBreakpointValue } from "@chakra-ui/react";
+
 import { Header } from "../components/Header";
 import { Banner } from "../components/Home/Banner";
 import { Divider } from "../components/Home/Divider";
+import { HomeSwiper } from "../components/Home/HomeSwiper";
 import { SectionTravel } from "../components/Home/SectionTravel";
 
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 
-import SwiperCore, { Pagination, Navigation } from "swiper";
 import { useEffect, useState } from "react";
 import { api } from "../services/api";
-SwiperCore.use([Pagination, Navigation]);
 
 interface ContinentProps {
   uid: string;
@@ -61,59 +59,7 @@ export default function Home() {
         </Text>
 
         <Box h="450px" mt={[5, 5, "3.25rem"]} mb={[6, 6, 10]}>
-          <Swiper
-            pagination={{
-              type: "bullets",
-            }}
-            loop={true}
-            navigation={true}
-          >
-            {continents.map((continent) => (
-              <SwiperSlide key={continent.name}>
-                <Link href={`/continent?uid=${continent.uid}`}>
-                  <a>
-                    <Image
-                      src={continent.image}
-                      alt={continent.name}
-                      style={{ filter: "brightness(.5)" }}
-                      w="100%"
-                      h="100%"
-                      backgroundSize="cover"
-                      backgroundRepeat="no-repeat"
-                      position="absolute"
-                      zIndex="hide"
-                    />
-                    <Box
-                      position="absolute"
-                      top="50%"
-                      left="50%"
-                      transform="auto"
-                      translateX="-50%"
-                      translateY="-50%"
-                      zIndex="base"
-                    >
-                      <Text
-                        color="light.100"
-                        fontWeight="bold"
-                        fontSize={["2xl", "2xl", "5xl"]}
-                        as="span"
-                      >
-                        {continent.name}
-                      </Text>
-                      <Text
-                        mt={[3, 3, 4]}
-                        color="light.300"
-                        fontWeight="bold"
-                        fontSize={["sm", "sm", "2xl"]}
-                      >
-                        O continente mais antigo.
-                      </Text>
-                    </Box>
-                  </a>
-                </Link>
-              </SwiperSlide>
-            ))}
-          </Swiper>
+          <HomeSwiper continents={continents} />
         </Box>
       </Box>
     </>
